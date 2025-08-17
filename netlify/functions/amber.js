2f4ae957cb0f85e84c0118846b4d0804888bc35a
king-chris123 <christasker123@gmail.com>
2025-08-18 04:11:43 +1000

    Update amber.js

--- netlify/functions/amber.js
+++ netlify/functions/amber.js
@@ -1,16 +1,11 @@
-// This secure function runs on the server and protects your API key.
+// This is a simple test function to make sure deployment works.
 export const handler = async () => {
-  const { AMBER_API_KEY, AMBER_SITE_ID } = process.env;
-  const AMBER_API_URL = `https://api.amber.com.au/v1/sites/${AMBER_SITE_ID}/prices/current`;
+  const testData = [{
+    perKwh: 15.7
+  }];
 
-  try {
-    const response = await fetch(AMBER_API_URL, {
-      headers: { 'Authorization': `Bearer ${AMBER_API_KEY}` },
-    });
-    if (!response.ok) throw new Error(`HTTP error ${response.status}`);
-    const data = await response.json();
-    return { statusCode: 200, body: JSON.stringify(data) };
-  } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(testData),
+  };
 };
